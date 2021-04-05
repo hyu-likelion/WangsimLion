4139da385bb0ad3f8f74ebc14669dac3b52ce58e
sgwon96 <zxcvb5434@likelion.org>
2021-04-05 17:50:58 +0900

    [gwon]2~4 주석

--- week1/gwon/2~4/problem3.py
+++ week1/gwon/2~4/problem3.py
@@ -1,24 +1,25 @@
-word = input().lower()
-s1 = set(word)
+word = input().upper()              # 단어를 word 변수에 입력 받고 전부 대문자로 변경 ex) Mississipi -> MISSISSIPI
+s1 = set(word)                      # 중복을 제거하기 위한 집합 s1 생성  ex) (M,I,S,P)
 
-dic = {}
+dic = {}                            # 비어 있는 딕셔너리 생성
 
-def get_key(val):
+def get_key(val):                   # 밸류값으로 키값을 찾는 함수
     for key, value in dic.items():
          if val == value:
              return key
 
-for s in s1 :
-    letCnt = word.count(s)
-    dic[s]=letCnt
+for s in s1 :                       # s1에 있는 문자들이 word에 몇개 들어있는지 체크
+    letCnt = word.count(s)          # count 함수를 사용 ex) letCnt = word.count("M") = 1
+    dic[s]=letCnt                   # 딕셔너리에 키값과 밸류값으로 문자와 문자 개수 입력 ex ) { "M" : 1 }
+                                    # {'I': 4, 'M': 1, 'S': 4, 'P': 1}
 
-cnts = list(dic.values())
+cnts = list(dic.values())           # dic에서 문자개수들을 리스트로 뽑기 ex) [4,1,4,1]
 
-max = max(cnts)
+max = max(cnts)                     # 문자 개수들 중 가장 큰 값 찾기  ex) max = 4
 
-maxCount = cnts.count(max)
+maxCount = cnts.count(max)          # 가장 많이 나온 문자가 여러개인지 확인 ex) maxCount = 2
 
-if maxCount == 1 :
-    print(get_key(max).upper())
-else :
+if maxCount == 1 :                  # 가장 많이 나온 문자가 하나일 경우 문자 출력 
+    print(get_key(max))             
+else :                              # 여러개일 경우 물음표 출력
     print("?")
--- week1/gwon/2~4/problem5.py
+++ week1/gwon/2~4/problem5.py
@@ -1,13 +1,16 @@
 def solution(phone_book):
-    check = phone_book[0]
-    del phone_book[0]
-    length = len(check)
-    answer = True
-
-    for num in phone_book :
-        if check == num[:length]:
-            answer = False
-    return answer
-
+    minNum = len(phone_book[0])                         # 가장 짧은 번호의 길이를 구하기 위해 초기화
+    
+    for num in phone_book:                              # 가장 짧은 번호의 길이를 구해서 minNum에 넣기
+        if minNum > len(num) :
+            minNum = len(num)
+    
+    for index in range(len(phone_book)):                # 가장 짧은 번호의 길이만큼 모든 번호 짜르기
+        phone_book[index] = phone_book[index][:minNum]
+    
+    s1 = set(phone_book)                                # 중복을 제거해주기 위해 집합으로 만들기 s1과 phone_book의 요소의 개수가 같으면 중복되는 번호가 없는 것
+    return len(s1)==len(phone_book)
 
 print(solution(["119", "97674223", "1195524421"]))
+print(solution(["123","456","789"]))
+print(solution(["12","123","1235","567","88"]))
