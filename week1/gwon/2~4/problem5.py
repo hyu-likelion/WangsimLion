d878f7390f9bc7fd91c3ee3e6fb9fc0e59f6cba2
SEUNGGWONYOON <67733995+sgwon96@users.noreply.github.com>
2021-04-06 21:26:44 +0900

    Update problem5.py

--- week1/gwon/2~4/problem5.py
+++ week1/gwon/2~4/problem5.py
@@ -1,16 +1,17 @@
-def solution(phone_book):
-    minNum = len(phone_book[0])                         # 가장 짧은 번호의 길이를 구하기 위해 초기화
-    
-    for num in phone_book:                              # 가장 짧은 번호의 길이를 구해서 minNum에 넣기
-        if minNum > len(num) :
-            minNum = len(num)
-    
-    for index in range(len(phone_book)):                # 가장 짧은 번호의 길이만큼 모든 번호 짜르기
-        phone_book[index] = phone_book[index][:minNum]
-    
-    s1 = set(phone_book)                                # 중복을 제거해주기 위해 집합으로 만들기 s1과 phone_book의 요소의 개수가 같으면 중복되는 번호가 없는 것
-    return len(s1)==len(phone_book)
+phone_book = ["123", "456", "789"]
+phone_book2 = ["12", "123", "1235", "567", "88"]
+phone_book3 = ["119", "97674223", "1195524421"]
 
-print(solution(["119", "97674223", "1195524421"]))
-print(solution(["123","456","789"]))
-print(solution(["12","123","1235","567","88"]))
+
+
+def solution(phone_book):
+    phone_book.sort()
+    
+    # 바로 뒤와 비교
+    # 오름차순 정렬되어 있으므로 어차피 바로 뒤에 것에 포함되어 있지 않으면 가망이 없다.
+    for i in range(len(phone_book) - 1):
+        val = phone_book[i]
+        compared_val = phone_book[i+1]
+        if val == compared_val[:len(val)]:
+            return False
+    return True
